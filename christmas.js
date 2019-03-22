dc2533eab1973846aff37cb7f456192b3b961101
Luke Dimas <dimasluke@gmail.com>
2019-03-22 14:31:09 -0700

    4 days added

--- christmas.js
+++ christmas.js
@@ -5,10 +5,10 @@ const christmas = {
     day4: 'coby',
     day5: 'zach',
     day6: 'steve',
-    day7: '',
-    day8: '',
-    day9: '',
-    day10: '',
+    day7: 'hello',
+    day8: 'world',
+    day9: 'javascript',
+    day10: 'react',
     day11: '',
     day12: ''
    };
