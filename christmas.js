3796169adea7ebc6985361e4ce09cffcba92cd74
Luke Dimas <dimasluke@gmail.com>
2019-03-22 14:27:34 -0700

    christmas.js

---
+++ christmas.js
@@ -0,0 +1,14 @@
+const christmas = {
+    day1: 'luke',
+    day2: 'bryce',
+    day3: 'daniel',
+    day4: 'coby',
+    day5: 'zach',
+    day6: 'steve',
+    day7: '',
+    day8: '',
+    day9: '',
+    day10: '',
+    day11: '',
+    day12: ''
+   };
