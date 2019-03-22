b1c3033a793127eb35fcc1ec12ad03f98f729f77
Daniel Szlauer <dszlauer@Daniels-MacBook-Pro.local>
2019-03-22 14:32:12 -0700

    added christmas

--- christmas.js
+++ christmas.js
@@ -1,14 +1,14 @@
 const christmas = {
-    day1: 'luke',
-    day2: 'bryce',
-    day3: 'daniel',
-    day4: 'coby',
-    day5: 'zach',
-    day6: 'steve',
-    day7: '',
-    day8: '',
-    day9: '',
-    day10: '',
-    day11: '',
-    day12: ''
-   };
+  day1: "luke",
+  day2: "bryce",
+  day3: "daniel",
+  day4: "coby",
+  day5: "zach",
+  day6: "steve",
+  day7: "Vodka",
+  day8: "Whiskey",
+  day9: "Rum",
+  day10: "Asprin",
+  day11: "",
+  day12: ""
+};
